b4ef310ffef3560ba1c5e146da92f04a56cbf75c
bbuchovecky <bgbucho@gmail.com>
2023-09-29 15:39:25 -0600

    edited PRECT script

--- calculate_PRECT_cesm_09.29.23.sh
+++ calculate_PRECT_cesm_09.29.23.sh
@@ -18,14 +18,17 @@ simtype="h0"
 srcpath="${srcdir}/${casename}/${gcomp}/hist"
 outpath="${outdir}/${casename}/proc/tseries"
 
-echo "Source: ${srcpath}"
-echo "Out:    ${outpath}"
-
 mkdir -p ${outpath}
-mkdir -p ${bashoutpath} 
+mkdir -p ${bashoutpath}
 
-cdo -setattribute,calculated_PRECT@long_name="Total precipitation rate (PRECC+PRECL)",calculated_PRECT@units="m/s" -chname,"PRECC","calculated_PRECT" -add -mergetime [ -select,name=PRECC [ ${srcpath}/${casename}.${scomp}.${simtype}.*.nc ] ] -mergetime [ -select,name=PRECL [ ${srcpath}/${casename}.${scomp}.${simtype}.*.nc ] ] ${outpath}/${casename}.${scomp}.${simtype}.timeseries.calculated_PRECT.nc |& tee ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "${now}" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "script:   $(basename "$0")" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "output:   ${outpath}/${casename}.${scomp}.${simtype}.timeseries.calculated_PRECT.nc" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "input:    ${srcpath}/${casename}.${scomp}.${simtype}.*.nc" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "casename: ${casename}" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "gcomp:    ${gcomp}" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "scomp:    ${scomp}" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "simtype:  ${simtype}" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+echo "" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
 
-echo "$(basename "$0")" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
-echo "input: ${srcpath}/${casename}.${scomp}.${simtype}.*.nc" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
-echo "output: ${outpath}/${casename}.${scomp}.${simtype}.timeseries.calculated_PRECT.nc" >> ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
+cdo -setattribute,calculated_PRECT@long_name="Total precipitation rate (PRECC+PRECL)",calculated_PRECT@units="m/s" -chname,"PRECC","calculated_PRECT" -add -mergetime [ -select,name=PRECC [ ${srcpath}/${casename}.${scomp}.${simtype}.*.nc ] ] -mergetime [ -select,name=PRECL [ ${srcpath}/${casename}.${scomp}.${simtype}.*.nc ] ] ${outpath}/${casename}.${scomp}.${simtype}.timeseries.calculated_PRECT.nc |& tee -a ${bashoutpath}/calculated_PRECT_${casename}_${now}.txt
